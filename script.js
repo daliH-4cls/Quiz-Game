551c4ab679091e8b2a08bfafc351b3dffdbbd345
daliH-4cls <daliah2rnndz10@gmail.com>
2026-04-23 16:27:12 +0000

    Majority of script added

--- script.js
+++ script.js
@@ -12,24 +12,60 @@ console.log("Script started");
 */
 
 // TODO: Create an array of objects to store the questions, options, and answer
+let questions = [
+    {
+        question: "What is the capital of Arizona?",
+        option1: "Tucson",
+        option2: "Phoenix",
+        option3: "Flagstaff",
+        option4: "Oracle",
+        answer: "Phoenix"
+    },
+    {
+        question: "What is the new dumb number that people are obsessed with?",
+        option1: "21",
+        option2: "420",
+        option3: "69",
+        option4: "67",
+        answer: "67"
+    }
+]
 
 // TODO: Declare an empty array that will hold the user's answers
+let userAnswers = [];
 
 // TODO: Declare a variable to keep track of which question (index) we are on
+let index = 0;
 
 function showNextQuestion() {
     // TODO: Get the current question
-    
+    let currQuestion = questions[index];
+
     // TODO: Set the text of the question element
+    let questionElement = document.getElementById("question");
+    questionElement.innerText = currQuestion.question;
 
     // TODO: Set the text of each option element
+    let option1Text = document.getElementById("option1text");
+    option1Text.innerText = currQuestion.option1;
+
+    let option2Text = document.getElementById("option2text");
+    option2Text.innerText = currQuestion.option2;
+
+    let option3Text = document.getElementById("option3text");
+    option3Text.innerText = currQuestion.option3;
+
+    let option4Text = document.getElementById("option4text");
+    option4Text.innerText = currQuestion.option4;
 }
 
 // TODO: Call showNextQuestion to load the first question when the page loads
+showNextQuestion();
 
 function submitAnswer(event) {
     console.log("Submit clicked");
     // TODO: Prevent the form from reloading the page
+    event.preventDefault();
 
     // Get the radio button input elements
     let option1 = document.getElementById("option1");
@@ -38,9 +74,12 @@ function submitAnswer(event) {
     let option4 = document.getElementById("option4");
 
     // TODO: Get the current question
+    let question = questions[index];
 
     // TODO: Use a conditional (if/else-if) to check which option was clicked and push the option text into the userAnswer array
-
+    if (option1.checked) {
+        console.log("Option1 picked");
+    }
     // TODO: Add one to the question index
 
     // TODO: Load the next question
